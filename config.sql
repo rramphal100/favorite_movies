7b41cf0d6948da094dc34fcb13c41808a691576c
Ryan Ramphal <rramphal94@gmail.com>
2016-06-24 02:15:36 -0400

    Added config.sql

---
+++ config.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS favorite_movies;
+CREATE DATABASE favorite_movies;
+
+\c favorite_movies
+
+CREATE TABLE users(
+username VARCHAR(15) PRIMARY KEY,
+password VARCHAR(10) NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES ('john', '12345');
+
+CREATE TABLE favorites(
+id SERIAL PRIMARY KEY,
+username VARCHAR(15) REFERENCES users(username),
+imdbid VARCHAR(12) NOT NULL,
+imgsrc TEXT NOT NULL,
+description TEXT
+);
+
+INSERT INTO favorites (username, imdbid, imgsrc, description) VALUES ('john', 'tt1905041', 
+	'http://ia.media-imdb.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg', 
+	'Hobbs has Dominic and Brian reassemble their crew to take down a team of mercenaries: Dominic unexpectedly gets convoluted also facing his presumed deceased girlfriend, Letty.');
